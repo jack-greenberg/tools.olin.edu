d89aecd35e4ae71269b1de53dfb8cdf4a0579a74
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-03-06 11:52:38 -0500

    Copy static files from CMS project

---
+++ static/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+module.exports =  {
+    mode: 'development',
+    entry: {
+        public: [
+            './public/js/index.js',
+            'babel-polyfill',
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '[name]/build'),
+    },
+    watch: true,
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        new WebpackNotifierPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        limit: 50000,
+                        outputPath: 'fonts/',
+                    }
+                },
+
+            },
+        ]
+    },
+};
