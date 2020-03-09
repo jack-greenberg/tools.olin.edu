1669d5659d7f2506042aafae6554c1e95a081b76
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-03-08 21:37:11 -0400

    Update init_db.sql with export from phpmyadmin

--- init_db.sql
+++ init_db.sql
@@ -1,29 +1,162 @@
-CREATE TABLE [IF NOT EXISTS] `tools` (
-	`id` int NOT NULL UNIQUE,
-	`name` varchar(255),
-	`worksheet` varchar(2083),
-	`reading` varchar(2083),
-	`prerequisites` int,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`prerequisites`) REFERENCES `tools`(`id`)
-)
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 08, 2020 at 06:35 PM
+-- Server version: 10.1.44-MariaDB-0+deb9u1
+-- PHP Version: 7.0.33-0+deb9u7
 
-CREATE TABLE [IF NOT EXISTS] `users` (
-	`id` int NOT NULL UNIQUE,
-	`external_id` int,
-	`first_name` varchar(255),
-	`last_name` varchar(255),
-	`email` varchar(255),
-	`role` varchar(255),
-	PRIMARY KEY (`id`),
-	CONSTRAINT chk_role CHECK (role in ('student', 'NINJA', 'staff', 'faculty', 'bow'))
-)
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-CREATE TABLE [IF NOT EXISTS] `users_tools` (
-	`used_id` int,
-	`tool_id` int,
-	`level` varchar(255),
-	CONSTRAINT chk_level CHECK (`level` in (NULL, 'fundamental', 'intermediate', 'cnc')),
-	FOREIGN KEY (`tool_id`) REFERENCES `tools`(`id`),
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
-)
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tools`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+		  `role_id` int(11) NOT NULL,
+		  `role_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`role_id`, `role_name`) VALUES
+(1, 'student'),
+(2, 'NINJA'),
+(3, 'faculty'),
+(4, 'staff'),
+(5, 'BOW');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tools`
+--
+
+CREATE TABLE `tools` (
+		  `id` int(11) NOT NULL,
+		  `name` varchar(255) NOT NULL,
+		  `prerequisite` int(11) DEFAULT NULL,
+		  `reading` varchar(2043) DEFAULT NULL,
+		  `worksheet` varchar(2043) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+		  `id` int(11) NOT NULL,
+		  `external_id` int(11) DEFAULT NULL,
+		  `first_name` varchar(255) NOT NULL,
+		  `last_name` varchar(255) NOT NULL,
+		  `email` varchar(255) NOT NULL,
+		  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_tools`
+--
+
+CREATE TABLE `users_tools` (
+		  `user_id` int(11) NOT NULL,
+		  `tool_id` int(11) NOT NULL,
+		  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `tools`
+--
+ALTER TABLE `tools`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `prerequisite` (`prerequisite`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `external_id` (`external_id`),
+  ADD KEY `role` (`role`);
+
+--
+-- Indexes for table `users_tools`
+--
+ALTER TABLE `users_tools`
+  ADD KEY `tool_id` (`tool_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `tools`
+--
+ALTER TABLE `tools`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tools`
+--
+ALTER TABLE `tools`
+  ADD CONSTRAINT `prerequisite` FOREIGN KEY (`prerequisite`) REFERENCES `tools` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `role` FOREIGN KEY (`role`) REFERENCES `roles` (`role_id`);
+
+--
+-- Constraints for table `users_tools`
+--
+ALTER TABLE `users_tools`
+  ADD CONSTRAINT `tool_id` FOREIGN KEY (`tool_id`) REFERENCES `tools` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
