803d15183c9cbd06d8791d5d866456b3cc4569ec
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-03-30 17:41:00 -0700

    Update backend for testing w/o database

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from config import DevelopmentConfig, ProductionConfig
-from modules.db import *
+from modules.db import db
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://tools.olin:Pursuit harvard pepper3;:jgreenberg-dev.olin.edu:3306"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://jackg:eminem answered primarily charity@10.0.1.11:3306/tools"
+    #  app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://tools.olin:Pursuit harvard pepper3;:jgreenberg-dev.olin.edu:3306"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db = init_db(app)
+    db.init_app(app)
 
     # Set up JSON Web Token (JWT) Authentication
     from flask_jwt_extended import JWTManager
--- modules/db.py
+++ modules/db.py
@@ -1,10 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 
-def init_db(app):
-    return SQLAlchemy(app)
+db = SQLAlchemy()
 
-class Tool(object):
-    pass
+class Tool(db.Model):
+    __tablename__ = "tools"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
 
-class User(object):
-    pass
+    def __repr__(self):
+        return '<Tool %r>' % self.name
+
+class User(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<User %r>' % self.name
