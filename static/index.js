f7ae466f4e8f2ab77c2078be498467af3c18f85a
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-04-06 15:01:13 -0700

    Add basic react file

--- static/index.js
+++ static/index.js
@@ -1,1 +1,31 @@
+// Import Styles
 import './index.scss';
+
+// Import React stuff
+import React from 'react-js';
+import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { ErrorBoundary } from './errorboundary';
+
+export const AppContext = React.createContext({});
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <AppContext.Provider value={this.state.data}>
+                <Router />
+            </AppContext.Provider>
+        );
+    };
+};
+
+var renderedApp = (
+    <ErrorBoundary>
+        <App />
+    </ErrorBoundary>
+);
+
+ReactDOM.render(renderedApp, document.getElementById("root"));
