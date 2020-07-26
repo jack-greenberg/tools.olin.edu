7804bd784b979be9c035d299deea7156eb8b5db2
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-07-25 21:07:45 -0700

    FIX: auth methods

--- tools/auth/azure.py
+++ tools/auth/azure.py
@@ -3,28 +3,15 @@ import logging
 import requests
 
 from msal import ConfidentialClientApplication
-from flask import session
+from flask import session, url_for
 
-from tools.errors import AppException
+from tools.errors import AuthException
 
-from tools.config import (
-    AZURE_APPLICATION_ID,
-    AZURE_TENANT_ID,
-    AZURE_CLIENT_SECRET,
-    AZURE_AUTHORITY,
-)
+from tools.config import AZURE_APPLICATION_ID, AZURE_CLIENT_SECRET, AZURE_AUTHORITY
 
 
-class AuthException(AppException):
-    pass
-
-
-logger = logging.getLogger("auth")
-
-if not all(
-    [AZURE_APPLICATION_ID, AZURE_TENANT_ID, AZURE_CLIENT_SECRET, AZURE_AUTHORITY]
-):
-    logger.warning("Running without Azure")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class AuthHandler(object):
@@ -32,23 +19,11 @@ class AuthHandler(object):
     A wrapper for AzureAD authentication using msal
     """
 
-    def __init__(
-        self,
-        application_id=AZURE_APPLICATION_ID,
-        tenant_id=AZURE_TENANT_ID,
-        client_secret=AZURE_CLIENT_SECRET,
-        authority=AZURE_AUTHORITY,
-    ):
-        self.application_id = application_id
-        self.tenant_id = tenant_id
-        self.client_secret = client_secret
-        self.authority = authority
-
     @property
     def client(self):
         if not hasattr(self, "azure_client"):
             self.azure_client = ConfidentialClientApplication(
-                AZURE_APPLICATION_ID,
+                client_id=AZURE_APPLICATION_ID,
                 authority=AZURE_AUTHORITY,
                 client_credential=AZURE_CLIENT_SECRET,
             )
@@ -58,31 +33,43 @@ class AuthHandler(object):
         return self.client.get_authorization_request_url(
             scopes or ["User.ReadBasic.All"],
             state=state or str(uuid.uuid4()),
-            # redirect_uri=url_for("tools.index", _external=True)
             redirect_uri="http://localhost:8000/api/token",
+            # url_for("tools.index", _external=True)
         )
-
-    def get_current_user(self, id):
-        selection = ["id", "displayName", "mail", "givenName", "surname"]
-        endpoint = "https://graph.microsoft.com/v1.0/me/{0}".format(id)
-
-        response = requests.get(
-            endpoint, params={"$select": ",".join(selection)}
-        ).json()
-
-        return response
 
     def get_token(self, code, scopes, redirect_uri):
         response = self.client.acquire_token_by_authorization_code(
             code, scopes=scopes, redirect_uri=redirect_uri
         )
+        if "error" in response:
+            raise AuthException("Error acquiring token: %s" % response.get("error"))
         return response
+
+    def get_logout_url(self):
+        return (
+            AZURE_AUTHORITY
+            + "/oauth2/v2.0/logout?post_logout_redirect_uri=http://localhost:8000"
+            + url_for("tools.index")
+        )
+
+    def get_current_user(self):
+        selection = ["id", "displayName", "mail", "givenName", "surname"]
+        response = requests.get(
+            "https://graph.microsoft.com/v1.0/me/",
+            params={"$select": ",".join(selection)},
+            headers=self.get_auth_headers(),
+        ).json()
+        return response
+
+    @staticmethod
+    def get_auth_headers():
+        return {"Authorization": "Bearer " + session["access_token"]}
 
 
 def authed(f):
     def wrapped(*args, **kwargs):
         if not session.get("user"):
-            raise AuthException("User not logged in", code=401)
+            raise AuthException("User not logged in")
         return f(*args, **kwargs)
 
     return wrapped
