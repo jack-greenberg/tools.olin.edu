3169306963ad39338c418d8faf7c21b86442bb40
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-07-25 21:08:38 -0700

    ADD: auth routes

--- tools/routes/auth.py
+++ tools/routes/auth.py
@@ -1,34 +1,56 @@
-from flask import Blueprint, request, current_app, session, url_for, redirect
+from flask import Blueprint, g, request, current_app, session, url_for, redirect
 
-from tools.errors import AppException
+from tools.errors import AuthException
+from tools.database.models import User
+from tools.utils import Role
 
 AUTH = Blueprint("auth", __name__, url_prefix="/auth")
-
-
-class AuthException(AppException):
-    pass
 
 
 @AUTH.route("/token")
 def get_token():
     if request.args.get("state") != session.get("state"):
         return redirect(url_for("tools.index"))  # No-op, goes back to Index page
+
     if "error" in request.args:
         raise AuthException(
-            "There was an error in the auth process: {}".format(
-                request.args.get("error")
-            )
+            "Exception in auth process: {}".format(request.args.get("error"))
         )
 
     if request.args.get("code"):
         result = current_app.auth.get_token(
             code=request.args.get("code"),
             scopes=["User.ReadBasic.All"],
-            # redirect_uri=url_for("tools.index"),
-            redirect_uri="http://localhost:8000/api/token",
+            redirect_uri="http://localhost:8000/api/token",  # url_for("tools.index"),
         )
+
         session["user"] = result.get("id_token_claims")
-        session["token"] = result.get("access_token")
+        session["access_token"] = result.get("access_token")
         session["refresh_token"] = result.get("refresh_token")
-    # return redirect(url_for("tools.index"))
-    return redirect("http://localhost:8000/api/token")
+
+        db_result = g.db_session.query(User).filter_by(
+            user_id=session["user"].get("id")
+        )
+
+        if not db_result:
+            current_user = current_app.auth.get_current_user()
+
+            new_user = User(
+                user_id=current_user.get("id"),
+                email=current_user.get("email"),
+                first_name=current_user.get("givenName"),
+                last_name=current_user.get("surname"),
+                role=Role.STUDENT,
+            )
+            g.db_session.add(new_user)
+            g.db_session.flush()
+
+    return redirect(
+        "http://localhost:8000/api/token"
+    )  # redirect(url_for("tools.index"))
+
+
+@AUTH.route("/logout")
+def logout():
+    session.clear()
+    return redirect(current_app.auth.get_logout_url())
--- tools/routes/graphql.py
+++ tools/routes/graphql.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, request, current_app, session, redirect, url_for
+from flask import Blueprint, request, current_app, session, redirect, url_for, g
 from flask_graphql import GraphQLView
 
 from tools.database.schemas import AppSchema
+from tools.database.models import User
+
+from tools.utils import Role
 
 API = Blueprint("api", __name__, url_prefix="/api")
 
@@ -24,5 +27,27 @@ def get_token():
             redirect_uri="http://localhost:8000/api/token",  # url_for("tools.index")
         )
         session["user"] = response.get("id_token_claims")
+        session["access_token"] = response.get("access_token")
+        session["refresh_token"] = response.get("refresh_token")
+
+        db_result = (
+            g.db_session.query(User)
+            .filter_by(user_id=session["user"].get("oid"))
+            .first()
+        )
+
+        print(db_result)
+        if not db_result:
+            current_user = current_app.auth.get_current_user()
+
+            new_user = User(
+                user_id=current_user.get("id"),
+                email=current_user.get("mail"),
+                first_name=current_user.get("givenName"),
+                last_name=current_user.get("surname"),
+                role=Role.STUDENT,
+            )
+            g.db_session.add(new_user)
+            g.db_session.flush()
     # return redirect(url_for("tools.index"))
     return redirect("http://localhost:8000/api/token")
