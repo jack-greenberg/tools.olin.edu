e1ed9ee5126626dbf4dc42d76a4a9eae2ece3f84
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-08-03 07:14:11 -0700

    FIX: syntax in config

--- tools/config.py
+++ tools/config.py
@@ -27,7 +27,7 @@ class ProductionConfig(BaseConfig):
 
 DATABASE_CONFIG = {
     "drivername": "postgresql",
-    "host": os.getenv("POSTGRES_HOST", "tools-db")
+    "host": os.getenv("POSTGRES_HOST", "tools-db"),
     "port": "5432",
     "username": os.getenv("POSTGRES_USER", "tools"),
     "password": os.getenv("POSTGRES_PASSWORD", "development"),
@@ -38,9 +38,7 @@ AZURE_APPLICATION_ID = os.getenv("AZURE_APPLICATION_ID", "")
 AZURE_TENANT_ID = os.getenv("AZURE_TENANT_ID", "")
 AZURE_CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET", "")
 AZURE_AUTHORITY = (
-    ("https://login.microsoftonline.com/" + AZURE_TENANT_ID)
-    if AZURE_TENANT_ID
-    else ""
+    ("https://login.microsoftonline.com/" + AZURE_TENANT_ID) if AZURE_TENANT_ID else ""
 )
 AZURE_ENABLED = all(
     [AZURE_APPLICATION_ID, AZURE_TENANT_ID, AZURE_CLIENT_SECRET, AZURE_AUTHORITY]
