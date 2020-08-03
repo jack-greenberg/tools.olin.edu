b9537fc34994d31c55c17a02742ca4c40e44aeac
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-08-02 23:05:36 -0700

    FIX: lint

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,11 +2,11 @@ import uuid
 import pytest
 from unittest.mock import patch
 
-from flask import session
 from msal import ConfidentialClientApplication
 
 from tools.auth import AuthHandler
 from tools.errors import AuthException
+
 
 def test_unauthed_homepage(app, client):
     """
@@ -33,7 +33,12 @@ def test_auth_client(app):
     assert isinstance(app.auth.client, ConfidentialClientApplication)
 
     with app.test_request_context():
-        assert app.auth.get_logout_url() == "https://login.microsoftonline.com/ff6254d0-4690-468a-a4ad-828ace3bb668/oauth2/v2.0/logout?post_logout_redirect_uri=http://localhost:8000/"
+        assert app.auth.get_logout_url() == (
+            "https://login.microsoftonline.com/"
+            "ff6254d0-4690-468a-a4ad-828ace3bb668"
+            "/oauth2/v2.0/logout?"
+            "post_logout_redirect_uri=http://localhost:8000/"
+        )
 
 
 def test_auth_process(app, client):
@@ -42,21 +47,34 @@ def test_auth_process(app, client):
     """
 
     with app.test_request_context():
-        with patch.object(AuthHandler, "get_auth_url", return_value="https://example.com") as mock_method:
+        with patch.object(
+            AuthHandler, "get_auth_url", return_value="https://example.com"
+        ) as mock_method:
             response = client.get("/")
             assert mock_method.called
             assert response.data == b"<a href='https://example.com'>Click to log in</a>"
 
         token = uuid.uuid4()
-        with patch.object(ConfidentialClientApplication, "acquire_token_by_authorization_code", return_value={"token": token}) as mock_method:
-            token_data = app.auth.get_token("mock_code", ["SomeScope.All"], "https://example.com")
+        with patch.object(
+            ConfidentialClientApplication,
+            "acquire_token_by_authorization_code",
+            return_value={"token": token},
+        ) as mock_method:
+            token_data = app.auth.get_token(
+                "mock_code", ["SomeScope.All"], "https://example.com"
+            )
             assert token_data.get("token") == token
 
 
 def test_auth_failure(app):
-    with patch.object(ConfidentialClientApplication, "acquire_token_by_authorization_code", return_value={"error": "some error"}) as mock_method:
+    with patch.object(
+        ConfidentialClientApplication,
+        "acquire_token_by_authorization_code",
+        return_value={"error": "some error"},
+    ):
         with pytest.raises(AuthException):
-            token_data = app.auth.get_token("mock_code", ["Scope"], "some_url")
+            app.auth.get_token("mock_code", ["Scope"], "some_url")
+
 
 @pytest.mark.skip
 def test_unauthed_path():
