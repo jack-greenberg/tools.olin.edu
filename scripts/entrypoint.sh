75fb81f2fc848e683cd21cedd489b6ba6a9c9509
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-08-14 22:42:05 -0700

    FIX: clean up alembic

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,7 +6,7 @@ until pg_isready -h ${POSTGRES_DB_NAME:-tools-db} -U ${POSTGRES_USER:-tools}; do
     sleep .5
 done
 
-# Import database stuff
+# Import database
 alembic upgrade head
 
 uwsgi uwsgi.ini &> /var/log/tools.log
